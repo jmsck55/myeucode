3013e0f695858fefeb581c7a5dee3954f92582c9
James Cook <jmsck55@gmail.com>
2023-03-13 12:32:43 -0400

    first commit

---
+++ source/main.c
@@ -0,0 +1,40 @@
+// Copyright (c) 2023 James Cook
+// Call Euphoria code by writing a script using "C", very fast.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define FILENAME_EVAL_EX "eval.ex"
+#define FILENAME_DATA_E "data.e"
+
+int main()
+{
+    clock_t t, t0;
+    int error_code;
+    FILE * pFile;
+
+    t0 = clock();
+
+    pFile = fopen(FILENAME_EVAL_EX, "w");
+    if (pFile == NULL)
+    {
+        puts("Couldn't open file.\n");
+        return 1;
+    }
+    fputs("--with trace\n--trace(1)\n",pFile);
+    fputs("puts(1, \"Hi world.\\n\")\n",pFile);
+    fprintf(pFile,"integer fn = open(\"%s\", \"w\")\n", FILENAME_DATA_E);
+    fputs("puts(fn, \"--with trace\\n--trace(1)\\n\")\n",pFile);
+    fprintf(pFile,"printf(fn, \"public object data = %%d\\n\", %d + %d)\n", 2, 2);
+    fputs("close(fn)\n",pFile);
+    fclose(pFile);
+
+    error_code = system("eui " FILENAME_EVAL_EX);
+
+    t = clock() - t0;
+    double sec = (double)(((double)t) / ((double)CLOCKS_PER_SEC));
+    printf("execution time in seconds\n%f\n", sec);
+
+    return 0;
+}
---
+++ source/main.ex
@@ -0,0 +1,37 @@
+-- Copyright (c) 2023 James Cook
+-- Call Euphoria code by writing a script using "Euphoria", not as fast as "C".
+
+--with trace
+
+--trace(1)
+
+constant FILENAME_EVAL_EX = "eval.ex"
+constant FILENAME_DATA_E = "data.e"
+
+integer fn
+
+object t, t0 = time()
+
+fn = open(FILENAME_EVAL_EX, "w")
+if fn = -1 then
+    puts(1, "Couldn't open file.\n")
+    abort(1/0)
+end if
+puts(fn, "--with trace\n--trace(1)\n")
+puts(fn, "puts(1, \"Hi world.\\n\")\n")
+printf(fn, "integer fn = open(\"%s\", \"w\")\n", {FILENAME_DATA_E})
+puts(fn, "puts(fn, \"--with trace\\n--trace(1)\\n\")\n")
+printf(fn, "printf(fn, \"public object data = %%d\\n\", %d + %d)\n", {2, 2})
+puts(fn, "close(fn)\n")
+close(fn)
+
+--trace(1)
+atom error_code
+error_code = system_exec("eui " & FILENAME_EVAL_EX, 2)
+? error_code
+
+t = time() - t0
+
+puts(1, "execution time in seconds:\n")
+? t
+
